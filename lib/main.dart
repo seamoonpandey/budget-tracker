f2305bcd1eb2fa815fddfd20aeb7594843e9cb8e
Seamoon Pandey <hackedasocial@gmail.com>
2023-07-29 17:59:07 +0545

    overwriting themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:budget_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData().copyWith(
         useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
       ),
       home: const Expenses(),
     ),
